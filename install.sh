4ec59b15a11ed1a7cd7c0ce58f9eb15267935d7a
force <force1267@gmail.com>
2020-04-25 06:50:31 +0430

    Fix deb/rpm  and node 14 to 13 in install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@ cd /home/force/biarbala
 if hash node 2>/dev/null; then
     echo "node exists"
 else
-    curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
     if hash yum 2>/dev/null; then # centos
+        curl -sL https://rpm.nodesource.com/setup_13.x | sudo bash -
         yum install -y nodejs gcc-c++ make
     elif hash apt 2>/dev/null; then # ubuntu
+        curl -sL https://deb.nodesource.com/setup_13.x | sudo bash -
         apt install -y nodejs build-essential # not tested
     fi
 fi
