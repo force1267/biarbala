259897302bf622f9a9ff37e39da921fb3d034cac
force <force1267@gmail.com>
2020-04-25 06:24:30 +0430

    Add gcc make

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if hash node 2>/dev/null; then
 else
     curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
     if hash yum 2>/dev/null; then # centos
-        yum install -y nodejs
+        yum install -y nodejs gcc-c++ make
     elif hash apt 2>/dev/null; then # ubuntu
-        apt install -y nodejs
+        apt install -y nodejs build-essential # not tested
     fi
 fi
 /usr/bin/npm install
