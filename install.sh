76d8198a25ddb42944182e76f017584701850ed4
force <force1267@gmail.com>
2020-04-25 06:11:49 +0430

    Add installing nodejs with yum and apt

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@ if hash node 2>/dev/null; then
     echo "node exists"
 else
     curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+    if hash yum 2>/dev/null; then # centos
+        yum install -y nodejs
+    elif hash apt 2>/dev/null; then # ubuntu
+        apt install -y nodejs
+    fi
 fi
-npm install
+/usr/bin/npm install
 cp /home/force/biarbala/biarbala.proxy.service /lib/systemd/system
 cp /home/force/biarbala/biarbala.index.service /lib/systemd/system
-sudo systemctl daemon-reload
+systemctl daemon-reload
 systemctl start biarbala.proxy
 systemctl start biarbala.index
 systemctl enable biarbala.proxy
