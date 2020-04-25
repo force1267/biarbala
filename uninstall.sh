b73304bf1de071042eae4c9ab3899acdfc2f1047
force <force1267@gmail.com>
2020-04-25 08:25:54 +0430

    Add removing ports at uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,12 @@ systemctl disable biarbala.index
 rm /lib/systemd/system/biarbala.proxy.service
 rm /lib/systemd/system/biarbala.index.service
 systemctl daemon-reload
+
+# close port 80 and 443
+if firewall-cmd node 2>/dev/null; then
+    firewall-cmd --zone=public --permanent --remove-port=80/tcp
+    firewall-cmd --zone=public --permanent --remove-port=443/tcp
+    firewall-cmd --reload
+else
+    echo "open ports 80 and 443 manually"
+fi
