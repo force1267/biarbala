7ac7a175b502b68d1f3c6780777d72a9517c688c
force <force1267@gmail.com>
2020-04-29 14:24:43 +0430

    Update index

--- index.js
+++ index.js
@@ -11,26 +11,20 @@ if (cluster.isMaster) {
     for (var i = 0; i < app_workers; i++) {
         cluster.fork();
     }
-    // cluster.on('exit', (worker, code, signal) => {
-    //     console.error({
-    //         code: code,
-    //         signal: signal,
-    //     }, 'worker died un-expectedly... restarting it.');
-    //     // Fork if a worker dies.
-    //     cluster.fork();
-    // });
+    cluster.on('exit', (worker, code, signal) => {
+        console.error({
+            code: code,
+            signal: signal,
+        }, 'worker died un-expectedly... restarting it.');
+        // Fork if a worker dies.
+        cluster.fork();
+    });
 } else {
-    // init express app
-    require('./util/express-app')
-    
     // setup express app
     require("./serve")
     require("./upload")
     require("./txt")
     require("./password")
-    
-    // setup greenlock
-    require("./util/greenlock")
     
     // setup http and https
     require("./server")
