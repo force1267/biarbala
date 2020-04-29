393372927b2b76ada7b6bc96b0a4d77bd3073154
force <force1267@gmail.com>
2020-04-29 05:10:53 +0430

    Add open port 3000 for acme challanges

--- install.sh
+++ install.sh
@@ -21,9 +21,10 @@ systemctl daemon-reload
 systemctl start biarbala.index
 systemctl enable biarbala.index
 
-# open port 80 and 443
+# open port 80 and 443 for serving and 3000 for acme
 if firewall-cmd node 2>/dev/null; then
     firewall-cmd --zone=public --permanent --add-port=80/tcp
+    firewall-cmd --zone=public --permanent --add-port=3000/tcp
     firewall-cmd --zone=public --permanent --add-port=443/tcp
     firewall-cmd --reload
 else
--- uninstall.sh
+++ uninstall.sh
@@ -8,9 +8,10 @@ systemctl disable biarbala.index
 rm /lib/systemd/system/biarbala.index.service
 systemctl daemon-reload
 
-# close port 80 and 443
+# close port 80, 443 and 3000
 if firewall-cmd node 2>/dev/null; then
     firewall-cmd --zone=public --permanent --remove-port=80/tcp
+    firewall-cmd --zone=public --permanent --remove-port=3000/tcp
     firewall-cmd --zone=public --permanent --remove-port=443/tcp
     firewall-cmd --reload
 else
